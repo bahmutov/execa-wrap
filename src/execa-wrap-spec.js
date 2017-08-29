e85ff4210ec9fc6e01ea9a74b0f6b66b386645ca
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-28 23:23:13 -0400

    initial

---
+++ src/execa-wrap-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const execaWrap = require('.')
+
+describe('execa-wrap', () => {
+  it('write this test', () => {
+    console.assert(execaWrap, 'should export something')
+  })
+})
