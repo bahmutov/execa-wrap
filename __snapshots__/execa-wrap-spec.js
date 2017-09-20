371b74a8fd2dac7c2ba07138899f19f95e1d6bb3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-20 14:49:10 -0700

    update windows snapshots
    use powershell

--- __snapshots__/execa-wrap-spec.js
+++ __snapshots__/execa-wrap-spec.js
@@ -1,41 +1,5 @@
-exports['execa-wrap ls 1'] = `
-  command: ls src
-  code: 0
-  failed: false
-  killed: false
-  signal: null
-  timedOut: false
-
-  stdout:
-  -------
-  execa-wrap-spec.js
-  index.js
-  -------
-  stderr:
-  -------
-  
-  -------
-  `
-
-exports['execa-wrap failing 1'] = `
-  command: boo src
-  code: ENOENT
-  failed: true
-  killed: false
-  signal: null
-  timedOut: false
-
-  stdout:
-  -------
-  
-  -------
-  stderr:
-  -------
-  
-  -------
-  `
-
 exports['execa-wrap windows ls 1'] = `
+
   command: ls src
   code: ENOENT
   failed: true
@@ -52,9 +16,11 @@ exports['execa-wrap windows ls 1'] = `
   'ls' is not recognized as an internal or external command,
   operable program or batch file.
   -------
-  `
+  
+`
 
 exports['execa-wrap windows failing 1'] = `
+
   command: boo src
   code: ENOENT
   failed: true
@@ -71,5 +37,5 @@ exports['execa-wrap windows failing 1'] = `
   'boo' is not recognized as an internal or external command,
   operable program or batch file.
   -------
-  `
-
+  
+`
