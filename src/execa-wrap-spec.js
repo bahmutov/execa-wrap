4119960e23aee1b0c9a12287f5bd4eed22bd0c53
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-20 14:29:40 -0700

    print outputs

--- src/execa-wrap-spec.js
+++ src/execa-wrap-spec.js
@@ -4,12 +4,21 @@
 const execaWrap = require('.')
 const snapshot = require('snap-shot-it')
 
+function print (s) {
+	console.log(s)
+	return s
+}
+
 describe('execa-wrap', () => {
   it('ls', () => {
-    return execaWrap('ls', ['src']).then(snapshot)
+    return execaWrap('ls', ['src'])
+    	.then(print)
+    	.then(snapshot)
   })
 
   it('failing', () => {
-    return execaWrap('boo', ['src']).then(snapshot)
+    return execaWrap('boo', ['src'])
+    	.then(print)
+    	.then(snapshot)
   })
 })
