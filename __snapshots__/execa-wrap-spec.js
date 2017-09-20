3ba63d6ee17dbead5f2d1127798d29267009d5f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-20 14:35:51 -0700

    fix windows tests

--- __snapshots__/execa-wrap-spec.js
+++ __snapshots__/execa-wrap-spec.js
@@ -35,3 +35,41 @@ exports['execa-wrap failing 1'] = `
   -------
   `
 
+exports['execa-wrap windows ls 1'] = `
+  command: ls src
+  code: ENOENT
+  failed: true
+  killed: false
+  signal: null
+  timedOut: false
+
+  stdout:
+  -------
+  
+  -------
+  stderr:
+  -------
+  'ls' is not recognized as an internal or external command,
+  operable program or batch file.
+  -------
+  `
+
+exports['execa-wrap windows failing 1'] = `
+  command: boo src
+  code: ENOENT
+  failed: true
+  killed: false
+  signal: null
+  timedOut: false
+
+  stdout:
+  -------
+  
+  -------
+  stderr:
+  -------
+  'boo' is not recognized as an internal or external command,
+  operable program or batch file.
+  -------
+  `
+
